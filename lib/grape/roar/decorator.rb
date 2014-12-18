70d0db5ecca6477c8553b0d44b59e47e3151b58f
Stan Bondi <stan@fixate.it>
2014-12-18 08:17:21 +0200

    Unused options param (rubocop)

--- lib/grape/roar/decorator.rb
+++ lib/grape/roar/decorator.rb
@@ -3,7 +3,7 @@ require 'roar/decorator'
 module Grape
   module Roar
     class Decorator < ::Roar::Decorator
-      def self.represent(object, options = {})
+      def self.represent(object, _options = {})
         new(object)
       end
     end
