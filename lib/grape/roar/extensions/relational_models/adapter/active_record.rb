902bca5234794edef76f410081c9502319d6e71a
David Stancu <dstancu@nyu.edu>
2017-06-07 21:04:54 -0400

    ok, clear direction for this now, just need to finish implementing mapper, write custom exceptions for all possible user definition fails, and then test the mapper! exciting!

--- lib/grape/roar/extensions/relational_models/adapter/active_record.rb
+++ lib/grape/roar/extensions/relational_models/adapter/active_record.rb
@@ -6,9 +6,7 @@ module Grape
           class ActiveRecord < Base
             include RelationalModels::Validator::ActiveRecord
 
-            valid_for do |klass|
-              klass < ::ActiveRecord::Base
-            end
+            valid_for { |klass| klass < ::ActiveRecord::Base }
 
             def collection_methods
               @collection_methods ||= %i(has_many has_and_belongs_to_many)
--- lib/grape/roar/extensions/relational_models/mapper.rb
+++ lib/grape/roar/extensions/relational_models/mapper.rb
@@ -5,24 +5,27 @@ module Grape
         class Mapper
           extend Forwardable
 
-          # ATTACK PLAN:
-          # test validator first, easiest
-          #
-          # Move these to the validator
-          # validate both properties and the available DSL methods
-          # at the same time!
-
           def initialize(entity, represented)
             @model_klass = represented.class
           end
 
-          # def decorate
-          #   config.each_pair do |relation, opts|
-          #     raise unless validator.send(
-          #       "#{opts[:relation_kind]}_valid?", relation
-          #     )
-          #   end
-          # end
+          def decorate
+            config.each_pair do |relation, opts|
+              raise unless validator.send(
+                "#{opts[:relation_kind]}_valid?", relation
+              )
+
+              next map_collection(
+                relation, opts
+              ) if adapter.collection_methods.include?(relation)
+
+              next map_single_entity(
+                relation, opts
+              ) if adapter.single_entity_methods.include?(relation)
+
+              raise 'No such relation supported'
+            end
+          end
 
           def_delegators :@config, :[], :[]=
 
@@ -30,14 +33,14 @@ module Grape
 
           attr_reader :entity, :model_klass
 
-          def map_single(relation, opts)
+          def map_single_entity(relation, opts)
           end
 
           def map_collection(relation, opts)
           end
 
-          def validator
-            @validator ||= Validator.for(model_klass)
+          def adapter
+            @adapter ||= Adapter.for(model_klass)
           end
         end
       end
