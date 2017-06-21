f417b06665db268aa8fa61aaa0a57ce537db728d
David Stancu <dstancu@nyu.edu>
2017-06-21 19:51:35 -0400

    thought i fixed this last night ???

--- lib/grape/roar/extensions/relational_models/mapper.rb
+++ lib/grape/roar/extensions/relational_models/mapper.rb
@@ -41,7 +41,7 @@ module Grape
           end
 
           def map_collection(relation, opts)
-            return entity.link_relation(relation, true) unless opts.fetch(:embedded, false)
+            return entity.link_relation(relation, true) if opts.fetch(:embedded, false)
             entity.collection(relation, opts)
           end
 
@@ -49,7 +49,6 @@ module Grape
             if adapter.collection_methods.include?(opts[:relation_kind])
               map_collection(relation, opts)
             elsif adapter.single_entity_methods.include?(opts[:relation_kind])
-              opts.merge!(:relation_is_collection, true)
               map_single_entity(relation, opts)
             else
               raise Exceptions::UnsupportedRelationError,
@@ -60,7 +59,7 @@ module Grape
           end
 
           def map_single_entity(relation, opts)
-            return entity.link_relation(relation) unless opts.fetch(:embedded, false)
+            return entity.link_relation(relation) if opts.fetch(:embedded, false)
             entity.property(relation, opts)
           end
 
