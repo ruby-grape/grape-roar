8d6dd8cf6a651e6914300e4f6a4b730239446bd7
David Stancu <dstancu@nyu.edu>
2017-06-21 22:08:44 -0400

    first draft at mongoid adapter

---
+++ lib/grape/roar/extensions/relational_models/adapter/mongoid.rb
@@ -0,0 +1,34 @@
+module Grape
+  module Roar
+    module Extensions
+      module RelationalModels
+        module Adapter
+          class Mongoid < Base
+            valid_for { |klass| klass < ::Mongoid::Document }
+
+            def collection_methods
+              @collection_methods ||= %i(
+                embeds_many has_many has_and_belongs_to_many
+              )
+            end
+
+            def name_for_represented(represented)
+              klass_name = if represented.instance_of?(
+                Mongoid::Relations::Targets::Enumerable
+              )
+                represented.klass.name
+              else
+                represented.class.name
+              end
+              klass_name.demodulize.pluralize.downcase
+            end
+
+            def single_entity_methods
+              @single_entity_methods ||= %i(has_one belongs_to embeds_one)
+            end
+          end
+        end
+      end
+    end
+  end
+end
