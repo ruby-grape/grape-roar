5d53fc1e5e9e2d55a81d10e65e9485cca05d22f1
David Stancu <dstancu@nyu.edu>
2017-10-26 13:11:16 -0400

    reproduce failing spec

--- spec/representer_spec.rb
+++ spec/representer_spec.rb
@@ -1,20 +1,50 @@
 # frozen_string_literal: true
 
-
+require 'pry'
 
 describe Grape::Roar do
   include_context 'Grape API App'
 
   context 'representer' do
-    before do
-      subject.get('/article/:id') do
-        Article.new(title: 'Lonestar', id: params[:id])
+    context 'with a known model' do
+      before do
+        subject.get('/article/:id') do
+          Article.new(title: 'Lonestar', id: params[:id])
+        end
+      end
+
+      it 'returns a hypermedia representation' do
+        get '/article/666'
+        expect(last_response.body).to eq '{"title":"Lonestar","id":"666","links":[{"rel":"self","href":"/article/666"}]}'
       end
     end
 
-    it 'returns a hypermedia representation' do
-      get '/article/666'
-      expect(last_response.body).to eq '{"title":"Lonestar","id":"666","links":[{"rel":"self","href":"/article/666"}]}'
+    context 'without an instance singleton' do 
+      context 'as nil' do 
+        before do 
+          subject.get('/article/:id') do
+            present(nil, with: ArticleRepresenter)
+          end
+        end
+
+        it 'returns null' do
+          get '/article/666'
+          expect(last_response.body).to eq('null')
+        end
+      end
+
+      context 'as another immediate value' do 
+        before do 
+          subject.get('/article/:id') do
+            present(5, with: ArticleRepresenter)
+          end
+        end
+
+        it 'returns null' do
+          get '/article/666'
+          expect(last_response.body).to eq('null')
+        end
+      end 
     end
   end
 end
