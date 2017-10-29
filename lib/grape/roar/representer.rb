1d45244850ebb23defabcc30d5ddef92b6c59a84
David Stancu <dstancu@nyu.edu>
2017-10-28 20:33:40 -0400

    add comment + remove extraneous .

--- lib/grape/roar/representer.rb
+++ lib/grape/roar/representer.rb
@@ -9,6 +9,7 @@ module Grape
 
       module ClassMethods
         def represent(object, _options = {})
+          # See https://github.com/ruby-grape/grape-roar/issues/16
           raise TypeError if object.singleton_class == object.class
           object.extend(self)
           object
